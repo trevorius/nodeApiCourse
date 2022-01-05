b1fb80f72a31376e7c6072d6815b0011343960d8
trevorius <trevorius45@gmail.com>
2022-01-05 16:01:42 -0500

    :sparkles: begining of api

--- app.js
+++ app.js
@@ -3,19 +3,52 @@ const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
-app.use((req, res, next) => {
-    console.log("URL: " + req.url);
-    next();
-})
+app.use(morgan('dev'));
 
-app.get('/api', (req, res) => {
-    res.send(
-         'Welcome to the API'
-    );
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+    },
+    {
+        id: 2,
+        name: 'Jane Doe',
+    },
+    {
+        id: 3,
+        name: 'Jack Doe',
+    },
+];
+
+app.get('/api/v1/members/:id', (req, res) => {
+    res.json(success(members[(req.params.id - 1)]));
 });
 
-app.get('/api/v1/books/:id', (req, res) => {
-    res.send(req.params)
+app.get('/api/v1/members', (req, res) => {
+
+    if(req.query.max !=undefined && req.query.max >0){
+        res.json(success(members.slice(0, req.query.max)));
+    } else if (req.query.max != undefined){
+        res.json(error("invalid max parameter"))
+    }
+    else res.json(success(members));
 });
+
+
+
+
 
 app.listen(3000, () => console.log('Server started on port 3000'));
+
+function success(result){
+    return {
+        status: 'success',
+        result: result
+    }
+}
+function error(message){
+    return {
+        status: 'error',
+        message: message
+    }
+}
